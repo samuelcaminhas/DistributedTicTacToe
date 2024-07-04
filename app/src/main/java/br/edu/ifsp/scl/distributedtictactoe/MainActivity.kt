f0f7fbe32a24f90caf34b63decb66423c3398690
samuelcaminhas <samuel.caminhas@aluno.ifsp.edu.br>
2024-07-04 00:48:02 -0300

    Screen created, and started to implementation the integrations of this project 💡

---
+++ app/src/main/java/br/edu/ifsp/scl/distributedtictactoe/MainActivity.kt
@@ -0,0 +1,39 @@
+package br.edu.ifsp.scl.distributedtictactoe
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.distributedtictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.startGameButton.setOnClickListener {
+
+        }
+    }
+
+
+    fun createNewGame (){
+        startGame()
+    }
+
+    fun startGame(){
+        startActivity(Intent(this, PlayActivity::class.java)
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/distributedtictactoe/PlayActivity.kt
@@ -0,0 +1,46 @@
+package br.edu.ifsp.scl.distributedtictactoe
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.distributedtictactoe.databinding.ActivityPlayBinding
+
+class PlayActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var binding: ActivityPlayBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.b0.setOnClickListener(this)
+        binding.b1.setOnClickListener(this)
+        binding.b2.setOnClickListener(this)
+        binding.b3.setOnClickListener(this)
+        binding.b4.setOnClickListener(this)
+        binding.b5.setOnClickListener(this)
+        binding.b6.setOnClickListener(this)
+        binding.b7.setOnClickListener(this)
+        binding.b8.setOnClickListener(this)
+
+
+        binding.StartGameBT.setOnClickListener{
+            startGame()
+        }
+
+    }
+
+    fun startGame(){
+        startActivity(Intent(this, PlayActivity::class.java))
+    }
+}
